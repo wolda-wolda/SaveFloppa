be1772b5ee1131e614b8c25e587389e8660e6625
Mud <skymudz@gmail.com>
2021-11-26 12:43:23 +0100

    Added Welcome Message

--- src/com/floppa/Main.java
+++ src/com/floppa/Main.java
@@ -2,6 +2,6 @@ package com.floppa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("Welcome to Epic Floppa Gaem :D");
     }
 }
