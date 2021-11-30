c74814a2f112de430ff31475db5e33d544a7d72c
Mud <skymudz@gmail.com>
2021-11-30 10:20:46 +0100

    Added epic Method to create a starting area. Added further Methods in the Room class to assist the process.

--- src/com/floppa/Main.java
+++ src/com/floppa/Main.java
@@ -3,5 +3,13 @@ package com.floppa;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Epic Floppa Gaem :D");
+        Room room = new Room();
+        addStartingArea(room);
+    }
+
+    public static void addStartingArea(Room room){
+        room.addRoom("east", new Room());
+        room.addRoom("west", new Room());
+        room.addRoom("north", new Room());
     }
 }
--- src/com/floppa/Room.java
+++ src/com/floppa/Room.java
@@ -8,4 +8,12 @@ public class Room {
     private ArrayList<Room> exit;
     private Info info;
     private HashMap<String, Room> rooms;
+
+    void addRoom(String key, Room room) {
+        rooms.put(key, room);
+    }
+
+    HashMap getRooms() {
+        return rooms;
+    }
 }
