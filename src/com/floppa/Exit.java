c1d2b9b4d777eb00d520623894a39de37ab60712
Marcel <marcelwalder12@gmail.com>
2021-11-30 09:58:12 +0100

    Create Initial Classes
    Signed-off-by: Marcel <marcelwalder12@gmail.com>

---
+++ src/com/floppa/Exit.java
@@ -0,0 +1,15 @@
+package com.floppa;
+
+public class Exit extends Room implements Interaction {
+    private Room room;
+    private boolean locked;
+
+    public void unlock() {
+
+    }
+
+    @Override
+    public void open() {
+        //makes open zhe door
+    }
+}
---
+++ src/com/floppa/WorldObject.java
@@ -0,0 +1,5 @@
+package com.floppa;
+
+public class WorldObject {
+    private Info info;
+}
