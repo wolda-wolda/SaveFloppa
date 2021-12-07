766a3872fec621a169d5b40b3dfd9cce1e6b272f
Marcel <marcelwalder12@gmail.com>
2021-12-07 10:20:57 +0100

    Renamed Position class to Pos
    Signed-off-by: Marcel <marcelwalder12@gmail.com>

--- src/com/floppa/Room/Exit.java
+++ src/com/floppa/Room/Exit.java
@@ -3,7 +3,6 @@ package com.floppa.Room;
 import com.floppa.Info;
 import com.floppa.Interaction;
 import com.floppa.Position.Position;
-import com.floppa.Room.Room;
 
 public class Exit extends Room implements Interaction {
     private Room room;
