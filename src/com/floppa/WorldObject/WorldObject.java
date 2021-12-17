f20f953c68855b4de742d73709eb7594ac9b9579
Mud <skymudz@gmail.com>
2021-12-17 12:21:20 +0100

    Added the ability to spawn multiple WorldObject with random content in them

--- src/com/floppa/WorldObject/WorldObject.java
+++ src/com/floppa/WorldObject/WorldObject.java
@@ -1,11 +1,30 @@
 package com.floppa.WorldObject;
 
 import com.floppa.Info;
+import com.floppa.Interaction;
+import com.floppa.Items.Item;
+
+import java.util.ArrayList;
 
 public class WorldObject {
     private Info info;
+    private ArrayList<Item> content = new ArrayList<>();
+    private boolean open = false;
 
     public WorldObject(Info info) {
         this.info = info;
+    }
+
+    public ArrayList<Item> open() {
+        if(!open) {
+            open = true;
+            return content;
+        }
+        System.out.println("This Container has already been opened. It's empty");
+        return null;
+    }
+
+    public void addItem(Item item) {
+        content.add(item);
     }
 }
