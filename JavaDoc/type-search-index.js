5fe1245a9a401bf40ec5b7417c2831b81a30e314
Marcel <marcelwalder12@gmail.com>
2022-01-11 10:03:35 +0100

    JavaCock
    Signed-off-by: Marcel <marcelwalder12@gmail.com>

---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,23 @@
+typeSearchIndex = [{"l": "All Classes and Interfaces", "u": "allclasses-index.html"}, {
+    "p": "com.floppa.WorldObject",
+    "l": "Chest"
+}, {"p": "com.floppa.WorldObject", "l": "Closet"}, {"p": "com.floppa.Entity", "l": "Enemy"}, {
+    "p": "com.floppa.Entity",
+    "l": "Entity"
+}, {"p": "com.floppa.Entity", "l": "Floppa"}, {"p": "com.floppa.Items", "l": "Food"}, {
+    "p": "com.floppa.Menu",
+    "l": "Help"
+}, {"p": "com.floppa", "l": "Info"}, {"p": "com.floppa", "l": "Interaction"}, {
+    "p": "com.floppa.Items",
+    "l": "Inventory"
+}, {"p": "com.floppa.Items", "l": "Item"}, {"p": "com.floppa.Items", "l": "Key"}, {
+    "p": "com.floppa",
+    "l": "Main"
+}, {"p": "com.floppa.Menu", "l": "MainMenu"}, {"p": "com.floppa.Menu", "l": "Menu"}, {
+    "p": "com.floppa.Entity",
+    "l": "Player"
+}, {"p": "com.floppa.Position", "l": "Pos"}, {"p": "com.floppa.Room", "l": "Room"}, {
+    "p": "Config.SaveGame",
+    "l": "SaveGame"
+}, {"p": "com.floppa.Menu", "l": "SoundPlayer"}, {"p": "com.floppa.WorldObject", "l": "WorldObject"}];
+updateSearchResults();
